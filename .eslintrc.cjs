111182f25897bd7a75a4ff25d5e544fc168ddb16
tim <njytim@gmail.com>
2025-12-09 22:00:40 +0800

    fix: resolve lint errors for CI pipeline
    - Exclude android/ios build artifacts from eslint
    - Add 'global' to eslint globals for test compatibility
    - All 95 errors resolved (only warnings remain)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
+    ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'android', 'ios'],
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
     settings: { react: { version: '18.2' } },
     plugins: ['react-hooks'],
@@ -21,6 +21,7 @@ module.exports = {
         beforeAll: 'readonly',
         afterAll: 'readonly',
         test: 'readonly',
+        global: 'readonly',
     },
     rules: {
         'react/prop-types': 'off',
